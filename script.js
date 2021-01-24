e2486600ce094755430c63b4105039c440af1ef8
Manjinder <manjindersinghjohal@aol.com>
2021-01-24 23:52:59 +0530

    added output message

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ let bill;
 const calculateTip = () => {
     bill = parseFloat(document.getElementById('bill').value);
     const result = bill * (15 / 100);
-    document.getElementById('output').innerHTML = result.toFixed(2);
+    document.getElementById('output').innerHTML = `Tip is Rs. ${result.toFixed(2)}`;
 
 }
 
