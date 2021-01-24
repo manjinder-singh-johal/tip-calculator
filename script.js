ac9dd0e6ac56ee0cff0ae51b8ac96d35e145ea73
Manjinder <manjindersinghjohal@aol.com>
2021-01-25 00:22:37 +0530

    added validation

--- script.js
+++ script.js
@@ -3,9 +3,15 @@ let bill;
 const calculateTip = () => {
     bill = parseFloat(document.getElementById('bill').value);
     const result = bill * (15 / 100);
-    document.getElementById('output').innerHTML = `Tip is Rs. ${result.toFixed(2)}`;
 
+    if (isNaN(result)) {
+        document.getElementById('output').innerHTML = `Please enter a valid number and try again.`
+    } else {
+        document.getElementById('output').innerHTML = `Tip is Rs. ${result.toFixed(2)}`;
+    }
+   
 }
+
 
 const reset = () => {
     document.getElementById('bill').value = '';
